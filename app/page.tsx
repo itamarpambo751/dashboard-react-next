c3200c79ef51846391505047fac97aad7b89b740
itamarpambo <itamarpambo751@gmail.com>
2024-03-05 21:08:21 +0100

    Feat: Dashboard structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,126 @@
-import Image from "next/image";
+import Main from "./fragments/_ui/Main";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <Main>
+      <div className="p-5">
+        <header className="w-full flex justify-between items-center">
+          <h1 className="font-bold text-xl">
+            Dashboard
+          </h1>
+          <div className="flex space-x-10 items-center">
+            <form className="flex bg-white overflow-hidden rounded-xl w-52 shadow">
+              <input type="text" placeholder="Search here..." className="p-3 w-10/12 outline-none" />
+              <button className="flex justify-center items-center flex-1">S</button>
+            </form>
+            <div className="flex space-x-4">
+              <button className="w-10 h-10 rounded-full bg-slate-800"></button>
+              <button className="w-10 h-10 rounded-full bg-slate-800"></button>
+              <button className="w-10 h-10 rounded-full bg-slate-800"></button>
+            </div>
+            <button className="bg-[var(--third)] text-white flex justify-center items-center overflow-hidden rounded-xl w-48 shadow p-3">
+              Generate Report
+            </button>
+          </div>
+        </header>
+        <div className="flex space-x-6 mt-12">
+          <div className="shadow rounded-3xl flex-1 flex space-x-4 h-36 gradient_1 p-6">
+            <div className="w-12 h-12 rounded-full bg-slate-300/60 flex justify-center items-center text-white">
+              Icon
+            </div>
+            <div>
+              <h1 className="text-2xl font-semibold text-white">2478</h1>
+              <span className="text-white">Total Invoices</span>
+            </div>
+          </div>
+          <div className="shadow rounded-3xl flex-1 flex space-x-4 h-36 gradient_3 p-6">
+            <div className="w-12 h-12 rounded-full bg-slate-300/60 flex justify-center items-center text-white">
+              Icon
+            </div>
+            <div>
+              <h1 className="text-2xl font-semibold text-white">983</h1>
+              <span className="text-white">Paid Invoices</span>
+            </div>
+          </div>
+          <div className="shadow rounded-3xl flex-1 flex space-x-4 h-36 gradient_1 p-6">
+            <div className="w-12 h-12 rounded-full bg-slate-300/60 flex justify-center items-center text-white">
+              Icon
+            </div>
+            <div>
+              <h1 className="text-2xl font-semibold text-white">1256</h1>
+              <span className="text-white">Unpaid Invoices</span>
+            </div>
+          </div>
+          <div className="shadow rounded-3xl flex-1 flex space-x-4 h-36 gradient_2 p-6">
+            <div className="w-12 h-12 rounded-full bg-slate-300/60 flex justify-center items-center text-white">
+              Icon
+            </div>
+            <div>
+              <h1 className="text-2xl font-semibold text-white">652</h1>
+              <span className="text-white">Total Invoices Sent</span>
+            </div>
+          </div>
         </div>
+        <section className="grid grid-cols-2 space-x-6 shadow rounded-3xl w-full h-[22rem] mt-8 p-6">
+          <div className="gradient_1 h-full rounded-3xl p-8 flex justify-between">
+            <div className="flex flex-col justify-end items-end">
+              <div className="flex flex-col space-y-2">
+                <h2 className="text-4xl font-medium text-white">$824,571.93</h2>
+                <span className="text-white">
+                  Wallet Balance
+                </span>
+                <span className="text-white">
+                  +0.8% than last week
+                </span>
+              </div>
+            </div>
+            <div className="h-full w-1/5 p-3 bg-white rounded-3xl flex justify-center items-center">
+              <h2 className="font-bold -rotate-90">Change</h2>
+            </div>
+          </div>
+          <div className="grid grid-cols-2">
+            <div>
+              <h1 className="font-bold mb-2 text-lg">Card´s Overview</h1>
+              <p className="text-gray-400 leading-5 text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quod a elit. Quo quod a</p>
+              <div className="mt-10">
+                <ul className="flex flex-col space-y-4">
+                  <li className="flex justify-between items-center">
+                    <div className="flex items-center space-x-2">
+                      <div className="w-4 h-4 rounded-full gradient_1" />
+                      <strong className="text-sm text-stone-700">Account</strong>
+                    </div>
+                    <p className="text-gray-400 text-md">20%</p>
+                  </li>
+                  <li className="flex justify-between items-center">
+                    <div className="flex items-center space-x-2">
+                      <div className="w-4 h-4 rounded-full gradient_2" />
+                      <strong className="text-sm text-stone-700">Services</strong>
+                    </div>
+                    <p className="text-gray-400 text-md">40%</p>
+                  </li>
+                  <li className="flex justify-between items-center">
+                    <div className="flex items-center space-x-2">
+                      <div className="w-4 h-4 rounded-full gradient_1" />
+                      <strong className="text-sm text-stone-700">Restaurant</strong>
+                    </div>
+                    <p className="text-gray-400 text-md">15%</p>
+                  </li>
+                  <li className="flex justify-between items-center">
+                    <div className="flex items-center space-x-2">
+                      <div className="w-4 h-4 rounded-full bg-stone-300" />
+                      <strong className="text-sm text-stone-700">Others</strong>
+                    </div>
+                    <p className="text-gray-400 text-md">15%</p>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="flex justify-center items-center">
+              <div className="w-48 h-48 rounded-full gradient_3" />
+            </div>
+          </div>
+        </section>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </Main>
   );
 }
